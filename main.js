ddaa9701c8374c925c4a0581f4e1b45794089ccb
Han Yi <ooidavid10@gmail.com>
2023-06-08 21:02:45 +0800

    azure

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 const MongoClient = require("mongodb").MongoClient;
-const BUS = require("./bus");
 const BUS_STOP = require("./bus_stop");
 
 const mqtt = require("mqtt");
 const broker = "mqtt://broker.emqx.io";
-const topic = "/RESTful";
+const topic = "camera-idp";
+
 //const client_id = "python-mqtt-" + Math.floor(Math.random() * 100);
-const client = mqtt.connect(broker, { clientId: "python-mqtt-500" });
-client.on("connect", () => {
-  console.log("Connected to MQTT Broker!");
-  client.subscribe(topic);
+const mqttclient = mqtt.connect(broker, { clientId: "nodejs" });
+mqttclient.on("connect", () => {
+  console.log("Connected to MQTT Broker");
+  mqttclient.subscribe(topic);
 });
 
-client.on("message", (topic, message) => {
+mqttclient.on("message", (topic, message) => {
   console.log(`Received '${message.toString()}' from '${topic}' topic`);
+  MongoInsert(message.toString());
 });
 
 MongoClient.connect("mongodb+srv://admin:admin@idp.3vyarrm.mongodb.net/test", {
@@ -26,39 +27,38 @@ MongoClient.connect("mongodb+srv://admin:admin@idp.3vyarrm.mongodb.net/test", {
   })
   .then(async (client) => {
     console.log("Connected to MongoDB");
-    BUS.injectDB(client);
     BUS_STOP.injectDB(client);
   });
 
 const express = require("express");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
-const cors = require("cors");
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+//const cors = require("cors");
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Connected Bus System API",
-      version: "1.0.0",
-    },
-    securityDefinitions: {
-      bearerAuth: {
-        type: "apiKey",
-        name: "Authorization",
-        scheme: "bearer",
-        in: "header",
-      },
-    },
-  },
-  apis: ["./main.js"],
-};
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Connected Bus System API",
+//       version: "1.0.0",
+//     },
+//     securityDefinitions: {
+//       bearerAuth: {
+//         type: "apiKey",
+//         name: "Authorization",
+//         scheme: "bearer",
+//         in: "header",
+//       },
+//     },
+//   },
+//   apis: ["./main.js"],
+// };
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors()); // Use this after the variable declaration
+//app.use(express.urlencoded({ extended: false }));
+//app.use(cors()); // Use this after the variable declaration
 
 // welcome
 app.get("/", function (req, res) {
@@ -68,71 +68,29 @@ app.get("/", function (req, res) {
 // Middleware Express for JWT
 // app.use(verifyToken);
 
-// bus.js
-app.patch("/gps", async (req, res) => {
-  console.log("Request Body : ", req.query);
-  let bus = await BUS.fetchGPS(
-    req.query.gps,
-    req.query.latitude,
-    req.query.longitude
-  );
-  if (bus != null) {
-    console.log("Bus GPS location Updated");
-    res.status(200).json(bus);
-  } else {
-    console.log("Bus GPS not found");
-    res.status(404).send("Bus GPS not found");
-  }
-});
-
-app.get("/bus", async (req, res) => {
-  console.log("Request Body : ", req.query);
-  let bus = await BUS.fetchLocation(req.query.bus_plate);
-  if (bus != null) {
-    console.log("Bus location found and displayed");
-    res.status(200).json(bus);
-  } else {
-    console.log("Bus location not found");
-    res.status(404).send("Bus location not found");
-  }
-});
-
 //bus_stop.js
-app.patch("/waiting", urlencodedParser, async (req, res) => {
-  console.log("Request Body : " + req.query);
-  let bs = await BUS_STOP.waiting(req.query.area, req.query.waiting);
-  if (bs != null) {
-    console.log("People waiting at bus stop is " + req.query.waiting);
-    res.status(200).json(bs);
-  } else {
-    console.log("Bus stop ID not found");
-    res.status(404).send("Bus stop ID not found");
-  }
-});
+async function MongoInsert(data) {
+  BUS_STOP.insert(data);
+  // if (bs != null) {
+  //   console.log("Bus stop document inserted");
+  //   res.status(200).json(bs);
+  // } else {
+  //   console.log("Bus stop not found");
+  //   res.status(404).send("Bus stop not found");
+  // }
+}
 
-app.post("/insert", async (req, res) => {
-  console.log("Request Body : ", req.query);
-  let bs = await BUS_STOP.insert(req.query.waiting);
-  if (bs != null) {
-    console.log("Bus stop document inserted");
-    res.status(200).json(bs);
-  } else {
-    console.log("Bus stop not found");
-    res.status(404).send("Bus stop not found");
-  }
-});
-
-app.get("/bustop", async (req, res) => {
-  console.log("Request Body : ", req.query);
-  let bs = await BUS_STOP.fetchLocation(req.query.area);
-  if (bs != null) {
-    console.log("Bus stop location found and displayed");
-    res.status(200).json(bs);
-  } else {
-    console.log("Bus stop location not found");
-    res.status(404).send("Bus stop location not found");
-  }
-});
+// app.get("/bustop", async (req, res) => {
+//   console.log("Request Body : ", req.query);
+//   let bs = await BUS_STOP.fetchLocation(req.query.area);
+//   if (bs != null) {
+//     console.log("Bus stop location found and displayed");
+//     res.status(200).json(bs);
+//   } else {
+//     console.log("Bus stop location not found");
+//     res.status(404).send("Bus stop location not found");
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Smart Bus Stop app is listening on port ${port}`);
